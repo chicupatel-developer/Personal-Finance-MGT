7ceab703c91635015933196fa25c943549569ceb
ankitjpatel2007 <ankitjpatel2007@hotmail.com>
2022-08-28 11:32:02 -0500

    react-cc-wip2

--- reactapp-pfm/src/components/CreditCard/CreditCard.jsx
+++ reactapp-pfm/src/components/CreditCard/CreditCard.jsx
@@ -30,7 +30,96 @@ const CreditCard = () => {
     allCCs();
   }, []);
 
-  return <div></div>;
+  const displayPayeeId = (cell, row) => {
+    return (
+      <div>
+        <span>
+          <i className={getPayeeIcon(row.payeeType)}></i> {cell}
+        </span>
+      </div>
+    );
+  };
+  const displayPayeeType = (cell, row) => {
+    return (
+      <div>
+        <span>{getPayeeTypeName(cell)}</span>
+      </div>
+    );
+  };
+  const displayBalance = (cell, row) => {
+    return (
+      <div>
+        {row.payeeType === 3 ? (
+          <span>
+            {Number(cell) > 0 ? (
+              <span style={{ color: "green" }}>
+                <b>{cell}</b>
+              </span>
+            ) : (
+              <span style={{ color: "red" }}>
+                <b>{cell}</b>
+              </span>
+            )}
+          </span>
+        ) : (
+          <span>{cell}</span>
+        )}
+      </div>
+    );
+  };
+
+  const columns = [
+    {
+      dataField: "payeeId",
+      text: "#",
+      sort: true,
+      formatter: (cell, row) => displayPayeeId(cell, row),
+    },
+    {
+      dataField: "payeeName",
+      text: "Payee",
+      sort: true,
+    },
+    {
+      dataField: "payeeACNumber",
+      text: "A/C No",
+      sort: true,
+    },
+    {
+      dataField: "payeeType",
+      text: "Type",
+      sort: true,
+      formatter: (cell, row) => displayPayeeType(cell, row),
+    },
+    {
+      dataField: "balance",
+      text: "Balance",
+      sort: true,
+      formatter: (cell, row) => displayBalance(cell, row),
+    },
+  ];
+  return (
+    <div className="container">
+      <div className="mainHeader">Payees</div>
+      <hr />
+      <div className="row">
+        <div className="col-md-12 mx-auto">
+          {creditcards && creditcards.length > 0 ? (
+            <BootstrapTable
+              bootstrap4
+              keyField="payeeId"
+              data={creditcards}
+              columns={columns}
+              pagination={paginationFactory({ sizePerPage: 5 })}
+              filter={filterFactory()}
+            />
+          ) : (
+            <div className="noCCs">No Credit-Cards!</div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreditCard;
